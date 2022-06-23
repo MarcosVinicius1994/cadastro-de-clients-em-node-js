cc1719269d88723db01a891ccae69aeec5100f00
marcos.nunes <marcosvtn@yahoo.com.br>
2022-06-23 15:00:56 -0300

    Logica para a montagem da query de atualizacao do client

--- src/shared/utils/get-client-util.js
+++ src/shared/utils/get-client-util.js
@@ -9,8 +9,28 @@ module.exports = {
         return query
     },
     async queryPostClient(context) {
-        let query = `insert into cadastros.Customers(Name, Address)`
+        let query = `INSERT INTO cadastros.Customers(Name, Address)`
         query += ` VALUES ('${context.name}', '${context.address}')`
         return query
     },
+    async queryDeleteClientId(context) {
+        let query = `DELETE FROM cadastros.Customers`
+        query += ` WHERE Id = ${context.id}`
+        return query
+    },
+    async queryPutClientId(context) {
+        let query = `UPDATE cadastros.Customers `
+        const queryParam = context.query
+        const bodyParam = context.body
+        if (bodyParam.name) {
+            query += ` SET Name = '${bodyParam.name}'`
+        }
+        if (bodyParam.name && bodyParam.address) {
+            query += `, Address = '${bodyParam.address}'`
+        }else if(bodyParam.address) {
+            query += ` SET Address = '${bodyParam.address}'`
+        }
+        query += ` WHERE Id = ${queryParam.id}`
+        return query
+    }
 }
